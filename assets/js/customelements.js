30642dbeb720fb0c18a701f9a89b38e052d6ce06
Matthias <halfmat101@gmail.com>
2026-02-04 16:31:30 -0700

    Custom Elements javascript (and git Test)

---
+++ assets/js/customelements.js
@@ -0,0 +1,43 @@
+class CustomHeader extends HTMLElement {
+    constructor() {
+        super();
+
+        this.innerHTML = `<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">Home</a>
+                <button class="
+                    navbar-toggler" 
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavbar">
+
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="projects.html">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                </li>
+                </ul>
+                </div>
+            </div>
+        </nav>`;
+    }
+}
+
+class CustomFooter extends HTMLElement {
+    constructor() {
+        super();
+
+        this.innerHTML = `<h3>This is the footer</h3>`;
+    }
+}
+
+customElements.define("custom-footer", CustomFooter);
+customElements.define("custom-header", CustomHeader);
