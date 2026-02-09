c1281576b9a01f5013e6902d04dc1a769d4c64ee
Matthias <halfmat101@gmail.com>
2026-02-09 08:21:06 -0700

    Navbar and Footer uses the custom elements on each page

--- assets/js/customelements.js
+++ assets/js/customelements.js
@@ -1,30 +1,26 @@
-class CustomHeader extends HTMLElement {
+class CustomNavbar extends HTMLElement {
     constructor() {
         super();
 
-        this.innerHTML = `<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        this.innerHTML = `
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.html">Home</a>
-                <button class="
-                    navbar-toggler" 
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavbar">
-
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="projects.html">Projects</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
-                </li>
-                </ul>
+                    <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="projects.html">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact</a>
+                    </li>
+                    </ul>
                 </div>
             </div>
         </nav>`;
@@ -35,9 +31,14 @@ class CustomFooter extends HTMLElement {
     constructor() {
         super();
 
-        this.innerHTML = `<h3>This is the footer</h3>`;
+        this.innerHTML = `
+        <footer class="bg-dark text-white mt-5 p-4">
+            <div class="container text-center">
+                &copy; 2026 SWDV 280 Portfolio Site
+            </div>
+        </footer>`;
     }
 }
 
 customElements.define("custom-footer", CustomFooter);
-customElements.define("custom-header", CustomHeader);
+customElements.define("custom-navbar", CustomNavbar);
