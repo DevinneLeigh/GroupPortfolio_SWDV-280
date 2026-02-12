003ed77bacc91fe1fbe71de9fa0a9fafd5f6a88d
Devinne Leigh <devinneleigh@gmail.com>
2026-02-11 17:20:42 -0700

    Add message confirm page.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -6,12 +6,15 @@ document.getElementById("contact-form")
 .addEventListener("submit", function(event) {
     event.preventDefault();
 
+    const button = this.querySelector("button");
+    button.disabled = true;
+    button.innerText = "Sending...";
+
     emailjs.sendForm("service_pd7jq6x", "template_1jwmn1d", this)
     .then(function() {
-        document.getElementById("status").innerText = "Message sent successfully!";
-        document.getElementById("contact-form").reset();
+        window.location.href = "message-confirm.html";
     }, function(error) {
+        console.log(error);
         document.getElementById("status").innerText = "Failed to send message.";
-          console.log(error);
     });
 });
